c226c425a014ca8a78a2df8a59126e3a64023b1f
Josh <hashimotojosh@gmail.com>
2024-02-08 23:54:27 -0800

    styled login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { authSuccess, authFail } from '../redux/authSlice';
 import { useNavigate } from 'react-router-dom';
-
+import { Box, VStack, Input, Button, FormControl, FormLabel, Heading, Text, useColorModeValue } from '@chakra-ui/react';
 const Login = () => {
   const [loginEmail, setLoginEmail] = useState('');
   const [loginPassword, setLoginPassword] = useState('');
@@ -39,15 +39,42 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      {errorMessage && <div className="text-red-500">{errorMessage}</div>}
-      <form onSubmit={handleSubmit}>
-        <input type="email" value={loginEmail} onChange={(e) => setLoginEmail(e.target.value)} placeholder="Email" />
-        <input type="password" value={loginPassword} onChange={(e) => setLoginPassword(e.target.value)} placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <Box className="flex flex-col items-center justify-center h-screen" bg={useColorModeValue('gray.50', 'gray.800')}>
+      <VStack spacing={8} mx="auto" w="lg" py={12} px={6}>
+        <Heading fontSize="3xl" textAlign="center">Log in to WorkHardStyle</Heading>
+        <Box rounded={'lg'} bg={useColorModeValue('white', 'gray.700')} boxShadow={'lg'} p={8} width="full" maxW="md">
+          <VStack spacing={4}>
+            <Button colorScheme="blue" w="full">Continue with Google</Button>
+            <Button colorScheme="facebook" w="full">Continue with Facebook</Button>
+            <Text pt={2} pb={2}>or</Text>
+            {errorMessage && <Text color="red.500">{errorMessage}</Text>}
+            <form onSubmit={handleSubmit} style={{ width: '100%' }}>
+              <VStack spacing={4}>
+                <FormControl id="email">
+                  <FormLabel>Email</FormLabel>
+                  <Input 
+                    type="email" 
+                    value={loginEmail} 
+                    onChange={(e) => setLoginEmail(e.target.value)} 
+                    placeholder="Enter your email" 
+                  />
+                </FormControl>
+                <FormControl id="password">
+                  <FormLabel>Password</FormLabel>
+                  <Input 
+                    type="password" 
+                    value={loginPassword} 
+                    onChange={(e) => setLoginPassword(e.target.value)} 
+                    placeholder="Enter your password" 
+                  />
+                </FormControl>
+                <Button type="submit" colorScheme="blue" w="full">Login</Button>
+              </VStack>
+            </form>
+          </VStack>
+        </Box>
+      </VStack>
+    </Box>
   );
 };
 
